5c9a0ed30975010e10d327647477647bb7ebc6c5
Sam Yeo <yeosamuel95@gmail.com>
2020-12-07 15:57:17 -0700

    Create test for user story 4

--- spec/features/roasters/index_spec.rb
+++ spec/features/roasters/index_spec.rb
@@ -13,17 +13,4 @@ RSpec.describe 'Index Page' do
         expect(page).to have_content(switchback.name)
         expect(page).to have_content(blue_bottle.name)
     end
-
-    it 'can see the id and its attributes' do 
-        stumptown = Roaster.create!(name: "Stumptown Coffee", micro: false, subscription_service: true, location: "Koreatown, NY")
-
-        visit "/roasters/#{stumptown.id}"
-
-        expect(page).to have_content (stumptown.name)
-        expect(page).to have_content (stumptown.micro)
-        expect(page).to have_content (stumptown.subscription_service)
-        expect(page).to have_content (stumptown.location)
-        expect(page).to have_content (stumptown.created_at)
-        expect(page).to have_content (stumptown.updated_at)
-    end
 end
---
+++ spec/features/roasters/show_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Show' do 
+    it "can show parent's content" do 
+        stumptown = Roaster.create!(name: "Stumptown Coffee", micro: false, subscription_service: true, location: "Koreatown, NY")
+
+        visit "/roasters/#{stumptown.id}"
+
+        expect(page).to have_content (stumptown.name)
+        expect(page).to have_content (stumptown.micro)
+        expect(page).to have_content (stumptown.subscription_service)
+        expect(page).to have_content (stumptown.location)
+        expect(page).to have_content (stumptown.created_at)
+        expect(page).to have_content (stumptown.updated_at)
+    end
+
+    it "can update the parent" do 
+        stumptown = Roaster.create!(name: "Stumptown Coffee", micro: false, subscription_service: true, location: "Koreatown, NY")
+        
+        visit "/roasters/#{stumptown.id}"
+
+        page.have_link "Edit", href: "/parents/#{stumptown.id}/edit"
+
+        fill_in 'Name', with: 'Edited'
+        fill_in 'micro', with: 'true'
+        fill_in 'subscription_service', with: 'true'
+        fill_in 'Location', with: 'Edited2'
+
+        click_on "Edit"
+
+        expect(page).to have_content('Edited')
+        expect(page).to have_content('true')
+        expect(page).to have_content('true')
+        expect(page).to have_content('Edited2')
+    end
+end
