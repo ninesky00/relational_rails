cc4fca97c1a2ed9b30373c25f4bdcfe13c13336f
Sam Yeo <yeosamuel95@gmail.com>
2020-12-09 22:23:08 -0700

    Complete Edit test for CoffeeBean Index Story 10

---
+++ spec/features/coffee_beans/delete_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Index' do
+    before :each do
+        @stumptown = Roaster.create!(name: "Stumptown Coffee", micro: false, subscription_service: true, location: "Koreatown, NY")
+        @starbuckspr = Roaster.create!(name: "Starbucks Private Reserve", micro: false, subscription_service: false, location: "Chelsea, NY")
+        @switchback = Roaster.create!(name: "Switchback Coffee", micro: true, subscription_service: false, location: "Colorado Springs ,CO")
+        @blue_bottle = Roaster.create!(name: "Blue Bottle Coffee", micro: false, subscription_service: true, location: "Midtown, NY")
+
+        @honduras_el_puente = @stumptown.coffee_beans.create!(name: "Honduras El Puente", weight: 12, blend: false, roast: "Light", country_of_origin: "Honduras")
+        @hair_bender = @stumptown.coffee_beans.create!(name: "Hair Bender", weight: 12, blend: false, roast: "Medium", country_of_origin: "N/A")
+        @costa_rica_naranjo = @starbuckspr.coffee_beans.create!(name: "Costa Rica Naranjo", blend: false, weight: 8, roast: "Medium", country_of_origin: "Costa Rica")
+        @institute_street_house = @switchback.coffee_beans.create!(name: "Institute Street House Blend", weight: 12, blend: true, roast: "Medium", country_of_origin: "N/A")
+        @colombia_s_f = @switchback.coffee_beans.create!(name: "Colombia San Fermin", weight: 80, blend: false, roast: "Light", country_of_origin: "Colombia")
+        @ethiopia_dammu = @switchback.coffee_beans.create!(name: "Ethiopia Dammu - Abana Coffee Estates", weight: 80, blend: false, roast: "Light", country_of_origin: "Ethiopia")
+        @winter_blooms_single = @blue_bottle.coffee_beans.create!(name: "Winter Blooms Single Origin", weight: 6, blend: false, roast: "Light", country_of_origin: "Ethiopia")
+        @winter_blooms_blend = @blue_bottle.coffee_beans.create!(name: "Winter Blooms Blend", weight: 6, blend: true, roast: "Medium", country_of_origin: "N/A")
+        @kirinyaga_kaguyu = @blue_bottle.coffee_beans.create!(name: "Kenya Kirinyaga Kaguyu", weight: 12, blend: false, roast: "Light", country_of_origin: "Kenya")
+        @santa_rosa_finca_santa_ana = @blue_bottle.coffee_beans.create!(name: "Guatemala Santa Rosa Finca Santa Ana", weight: 12, blend: false, roast: "Dark", country_of_origin: "Guatemala")
+    end
+    it "can delete CoffeeBean entry from index page" do
+        visit "/coffeebeans/#{@institute_street_house.id}"
+        click_button "Delete"
+
+        expect(page).not_to have_content(@institute_street_house.name)
+    end
+    it "can see update link and fill in update form" do
+        visit "/coffeebeans/#{@costa_rica_naranjo.id}"
+
+        click_link 'Update Coffee Bean'
+  
+        expect(current_path).to eq("/coffeebeans/#{@costa_rica_naranjo.id}/edit")
+  
+        fill_in 'Name', with: 'What the Dog Saw'
+        fill_in 'publication_date', with: "2013-01-01"
+        fill_in 'Pages', with: 410
+        fill_in 'currently_printed', with: true
+  
+        click_on "Update Book"
+  
+        expect(current_path).to eq("/books")
+        expect(page).to have_content("Name")
+        expect(page).to have_content("2013-01-01")
+        expect(page).to have_content(410)
+        expect(page).to have_content(true)
+    end
+end
---
+++ spec/features/coffee_beans/edit_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'Edit' do
+    before :each do
+        @stumptown = Roaster.create!(name: "Stumptown Coffee", micro: false, subscription_service: true, location: "Koreatown, NY")
+        @starbuckspr = Roaster.create!(name: "Starbucks Private Reserve", micro: false, subscription_service: false, location: "Chelsea, NY")
+        @switchback = Roaster.create!(name: "Switchback Coffee", micro: true, subscription_service: false, location: "Colorado Springs ,CO")
+        @blue_bottle = Roaster.create!(name: "Blue Bottle Coffee", micro: false, subscription_service: true, location: "Midtown, NY")
+
+        @honduras_el_puente = @stumptown.coffee_beans.create!(name: "Honduras El Puente", weight: 12, blend: false, roast: "Light", country_of_origin: "Honduras")
+        @hair_bender = @stumptown.coffee_beans.create!(name: "Hair Bender", weight: 12, blend: false, roast: "Medium", country_of_origin: "N/A")
+        @costa_rica_naranjo = @starbuckspr.coffee_beans.create!(name: "Costa Rica Naranjo", blend: false, weight: 8, roast: "Medium", country_of_origin: "Costa Rica")
+        @institute_street_house = @switchback.coffee_beans.create!(name: "Institute Street House Blend", weight: 12, blend: true, roast: "Medium", country_of_origin: "N/A")
+        @colombia_s_f = @switchback.coffee_beans.create!(name: "Colombia San Fermin", weight: 80, blend: false, roast: "Light", country_of_origin: "Colombia")
+        @ethiopia_dammu = @switchback.coffee_beans.create!(name: "Ethiopia Dammu - Abana Coffee Estates", weight: 80, blend: false, roast: "Light", country_of_origin: "Ethiopia")
+        @winter_blooms_single = @blue_bottle.coffee_beans.create!(name: "Winter Blooms Single Origin", weight: 6, blend: false, roast: "Light", country_of_origin: "Ethiopia")
+        @winter_blooms_blend = @blue_bottle.coffee_beans.create!(name: "Winter Blooms Blend", weight: 6, blend: true, roast: "Medium", country_of_origin: "N/A")
+        @kirinyaga_kaguyu = @blue_bottle.coffee_beans.create!(name: "Kenya Kirinyaga Kaguyu", weight: 12, blend: false, roast: "Light", country_of_origin: "Kenya")
+        @santa_rosa_finca_santa_ana = @blue_bottle.coffee_beans.create!(name: "Guatemala Santa Rosa Finca Santa Ana", weight: 12, blend: false, roast: "Dark", country_of_origin: "Guatemala")
+    end
+    it "can see update link and fill in update form" do
+        visit "/coffeebeans/#{@costa_rica_naranjo.id}"
+
+        click_link 'Update Coffee Bean'
+  
+        expect(current_path).to eq("/coffeebeans/#{@costa_rica_naranjo.id}/edit")
+  
+        fill_in 'Name', with: 'costa rica'
+        fill_in 'roast', with: "Dark"
+        fill_in 'country_of_origin', with: "test"
+        fill_in 'blend', with: "true"
+        fill_in 'weight', with: "12"
+  
+        click_on "Edit Coffee Bean"
+  
+        expect(current_path).to eq("/coffeebeans/#{@costa_rica_naranjo.id}")
+        expect(page).to have_content("costa rica")
+        expect(page).to have_content("Dark")
+        expect(page).to have_content("true")
+        expect(page).to have_content("12")
+    end
+end
